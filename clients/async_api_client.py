52e872a1ef45743c166c14d303416531f3aa95c4
AlantinAlAman <caladin95@mail.ru>
2023-03-16 00:37:04 +0300

    localization

--- clients/async_api_client.py
+++ clients/async_api_client.py
@@ -8,12 +8,13 @@ from clients.api_client import Headers
 
 class AsyncApiClient:
 
-    def __init__(self, auth: dict[str, Any], cookies: dict[str, str] | Cookies, timeout: int = 600,
+    def __init__(self, config: dict[str, Any], cookies: dict[str, str] | Cookies, timeout: int = 600,
                  verify: bool = False, cert: tuple[str, str] | None = None, follow_redirects: bool = True) -> None:
         self.verify = verify
         self.cookies = cookies
         self.timeout = timeout
-        self.base_url = f'{auth["protocol"]}{auth["url"]}' if not verify else f'{auth["protocol_ssl"]}{auth["url_ssl"]}'
+        self.base_url = f'{config["protocol"]}{config["url"]}' if not verify \
+            else f'{config["protocol_ssl"]}{config["url_ssl"]}'
         self.cert = cert
         self.follow_redirects = follow_redirects
         self.latest_request = None
@@ -28,7 +29,7 @@ class AsyncApiClient:
     def get_cookies(self) -> dict[str, str]:
         return self.cookies
 
-    # TODO: Добавить хук для аллюра
+    # TODO: Add a hook for allure
     async def __aenter__(self) -> AsyncApiClient:
         return self
 
@@ -76,16 +77,16 @@ class AsyncApiClient:
         return f
 
     def check_status_code(self, status_code: int | None) -> None:
-        """Метод для проверки статус кода"""
-        with allure.step('Проверка статуса кода запроса'):
+        with allure.step('Checking the status of the request code'):
             if not status_code:
-                assert self.latest_request, 'Произошла ошибка при выполнении запроса.' \
-                                            f' Status code: {self.latest_request.status_code}' \
-                                            f' message: {self.latest_request.text},'
+                assert self.latest_request, f'''An error occurred while executing the request. 
+                                                Status code: {self.latest_request.status_code}. 
+                                                Message: {self.latest_request.text}'''
             else:
                 assert self.latest_request.status_code == status_code, \
-                    f'Неправильный статус код, ожидался: {status_code} , ' \
-                    f'получен: {self.latest_request.status_code} , message: {self.latest_request.text}'
+                    f'''Invalid status code, expected: {status_code}. 
+                        Received: {self.latest_request.status_code}. 
+                        Message: {self.latest_request.text}'''
 
     async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
         await self.client.aclose()
