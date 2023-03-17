9c89d1902e34a678a5a77d2dec54f1b99af2f085
AlantinAlAman <caladin95@mail.ru>
2023-03-18 00:07:46 +0300

    example of api test based on jsonschema

---
+++ api_tests_examples/jsonschema/conftest.py
@@ -0,0 +1,23 @@
+﻿# System
+import json
+import os
+import pytest
+from _pytest.fixtures import SubRequest
+
+from http import HTTPStatus
+
+from typing import Any
+
+
+@pytest.fixture(scope='module')
+def get_base_schema(status_code: HTTPStatus, get_project_root: str) -> dict[str, Any]:
+    """Базовая схема данных для списка элементов в поле data"""
+    with open(os.path.join(get_project_root, os.path.dirname(__file__), 'base.schema.json'), 'rb') as f:
+        return json.load(f).get(str(status_code))
+
+
+@pytest.fixture(scope='module')
+def get_definitions_schema(request: SubRequest, status_code: HTTPStatus) -> dict[str, Any]:
+    """Фикстура, которая возвращает схему данных"""
+    with open(os.path.join(request.fspath.dirname, "test_/definitions.schema.json"), 'rb') as f:
+        return json.load(f).get(str(status_code))
